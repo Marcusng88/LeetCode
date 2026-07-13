b57cee89b2b9d3c81a8ce56cca5b8c1f693146d0
Marcusng88 <ngzhengjie888@gmail.com>
2026-07-13 17:32:25 +0800

    Time: 1 ms (99.97%) | Memory: 77.3 MB (32.05%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,14 +3,20 @@ class Solution {
         if(nums.length == 1) return nums[0];
         if(nums.length<1) return 0;
 
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        int max = dp[0];
-        for(int i =1; i<dp.length; i++){
-            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
-            if(dp[i]>max){
-                max = dp[i];
-            }
+        // int[] dp = new int[nums.length];
+        // dp[0] = nums[0];
+        // int max = dp[0];
+        // for(int i =1; i<dp.length; i++){
+        //     dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
+        //     if(dp[i]>max){
+        //         max = dp[i];
+        //     }
+        // }
+        int currentMax = nums[0];
+        int max = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            currentMax = Math.max(currentMax+nums[i], nums[i]);
+            max = Math.max(currentMax, max);
         }
 
         return max;
