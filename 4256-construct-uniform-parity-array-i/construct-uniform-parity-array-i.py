47ca590c8296dd7f8b38f120f72dc822134a71ca
Marcusng88 <ngzhengjie888@gmail.com>
2026-09-03 08:20:05 +0800

    Time: 3 ms (12.53%) | Memory: 19.4 MB (13.28%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,53 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # first intuition brute force
+        n = len(nums1)
+        nums2 = []
+        odd = True
+        even = True
+        # odd
+        for i in range(n):
+            curr = nums1[i]
+            if curr %2 != 0:
+                nums2.append(curr)
+                continue
+            
+            for j in range(n):
+                if j == i :
+                    continue
+                second = curr - nums1[j]
+                if second % 2 != 0:
+                    nums2.append(curr)
+                    break
+                if j == n:
+                    even = False
+        
+        nums2.clear()
+        # even
+        for i in range(n):
+            curr = nums1[i]
+            if curr %2 == 0:
+                nums2.append(curr)
+                continue
+            
+            for j in range(n):
+                if j == i :
+                    continue
+                second = curr - nums1[j]
+                if second % 2 == 0:
+                    nums2.append(curr)
+                    break
+                if j == n:
+                    even = False
+
+
+
+            
+
+        return odd or even
+        # sum = 0
+        # if len(nums1) == 1:
+        #     return True
+        # for i in nums1 :
+        #     sum+=1
+        # return sum % 2 == 0
